79cd4864927cafbc6826fc20270f3b4d8b75a2de
CoderJJ <1325964835@qq.com>
2020-07-16 19:02:36 +0800

    '配置文件'

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.export = {
+  configureWebpack:{
+    resolve:{
+     alias:{//配置别名
+       'assets': '@/assets',
+       'common':'@/common',
+       'components':'@/components',
+       'network':'@/network',
+       'views':'@/views'
+     } 
+    }
+  }
+}
