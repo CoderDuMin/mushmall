e597d536d57bd70e358460f972905f861a5fe11d
CoderJJ <1325964835@qq.com>
2020-07-17 10:41:02 +0800

    navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">魔姑街</div>
+    </nav-bar>
+    <h2>首页</h2>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-
+  components:{
+    NavBar
+  }
 }
 </script>
 
 <style>
-
+  .home-nav{
+    background: var(--color-tint);
+    color: #fff;
+  }
 </style>
